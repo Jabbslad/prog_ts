8034d8ee041f96cc5f7ecdf93b2b3dd101c109de
Jamie Atkinson <jabbslad@gmail.com>
2020-01-25 23:54:28 +0000

    add person class

--- chapter-2/src/object.ts
+++ chapter-2/src/object.ts
@@ -2,3 +2,9 @@ let f: { firstname: string; surname: string } = {
   firstname: "jabbs",
   surname: "lad"
 };
+
+class Person {
+  constructor(public firstname: string, public surname: string) {}
+}
+
+f = new Person("jabbs", "lad");
