bd1e4f6af13d299f15ce8d68dcee6b30cb408baa
Jamie Atkinson <jabbslad@gmail.com>
2020-01-26 00:46:05 +0000

    add type union (|)

--- chapter-2/src/object.ts
+++ chapter-2/src/object.ts
@@ -35,3 +35,17 @@ type Colour = "red";
 }
 let x: Colour = "red";
 console.log(x);
+
+type NumOrString = number | string;
+let j: NumOrString[] = [1, "2"];
+j.push(3);
+console.log(j);
+
+let k = [1, "2", 3, "four"];
+k = k.map(n => {
+  if (typeof n === "number") {
+    return n * 3;
+  }
+  return n.toUpperCase();
+});
+console.log(k);
