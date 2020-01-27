663575adf1d0810658f52bd9dba708bb143d50be
Jamie Atkinson <jabbslad@gmail.com>
2020-01-27 22:48:37 +0000

    add different function declarations

--- chapter-4/src/index.ts
+++ chapter-4/src/index.ts
@@ -3,3 +3,21 @@ function add(a: number, b: number): number {
 }
 
 console.log(add(4, 5));
+
+function greet(name: string): string {
+  return "hello " + name;
+}
+
+let greet2 = function(name: string): string {
+  return "hello " + name;
+};
+
+let greet3 = (name: string): string => {
+  return "hello " + name;
+};
+
+let greet4 = (name: string): string => "hello " + name;
+
+let greet5 = new Function("name", 'return "hello " + name');
+
+console.log(greet5("Jabbslad"));
