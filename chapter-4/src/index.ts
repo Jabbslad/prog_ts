bd8a1fb67117909b0a7e3dd00540986a040d6980
Jamie Atkinson <jabbslad@gmail.com>
2020-01-29 23:56:29 +0000

    added implementation of bounded polymorphism

--- chapter-4/src/index.ts
+++ chapter-4/src/index.ts
@@ -208,3 +208,22 @@ type LeafNode = TreeNode & {
 type InnerNode = TreeNode & {
   children: [TreeNode] | [TreeNode, TreeNode];
 };
+
+let a: TreeNode = { value: "a" };
+let b: LeafNode = { value: "b", isLeaf: true };
+let c: InnerNode = { value: "c", children: [b] };
+
+function mapNode<T extends TreeNode>(node: T, f: (value: string) => string): T {
+  return {
+    ...node,
+    value: f(node.value)
+  };
+}
+
+let a1 = mapNode(a, _ => _.toUpperCase());
+let b1 = mapNode(b, _ => _.toUpperCase());
+let c1 = mapNode(c, _ => _.toUpperCase());
+
+console.log(a1);
+console.log(b1);
+console.log(c1);
