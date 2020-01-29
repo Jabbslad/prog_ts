1e69b885ce13d4ec531bda080b98e06e4627688c
Jabbslad <jabbslad@gmail.com>
2020-01-29 18:59:13 +0000

    add untyped map function

--- chapter-4/src/index.ts
+++ chapter-4/src/index.ts
@@ -148,3 +148,13 @@ let filter: Filter<number> = (array, f) => {
   return result
 }
 console.log(filter([1, 2, 3, 4, 5], _ => _ < 3))
+
+// MAP
+
+function map(array: unknown[], f: (item: unknown) => unknown): unknown[] {
+  let result = []
+  for (let i = 0; i < array.length; i++) {
+    result[i] = f(array[i])
+  }
+  return result
+}
