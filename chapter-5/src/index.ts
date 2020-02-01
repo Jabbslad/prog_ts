0d469ed859d113a5972b4619623e5f81d41b6e71
Jamie Atkinson <jabbslad@gmail.com>
2020-02-01 23:21:00 +0000

    added with withEZDebug decorator

--- chapter-5/src/index.ts
+++ chapter-5/src/index.ts
@@ -159,3 +159,38 @@ map1.set(1, "one");
 map1.set(2, "two");
 
 console.log(map1);
+
+// Mixins
+
+type ClassConstructor<T> = new (...args: any[]) => T;
+
+function withEZDebug<C extends ClassConstructor<{ getDebugValue(): object }>>(
+  Class: C
+) {
+  return class extends Class {
+    debug() {
+      let Name = Class.constructor.name;
+      let value = this.getDebugValue();
+      return Name + "(" + JSON.stringify(value) + ")";
+    }
+  };
+}
+
+class HardToDebugUser {
+  constructor(
+    private id: number,
+    private firstName: string,
+    private lastName: string
+  ) {}
+
+  getDebugValue() {
+    return {
+      id: this.id,
+      name: this.firstName + " " + this.lastName
+    };
+  }
+}
+
+let User = withEZDebug(HardToDebugUser);
+let user1 = new User(1, "Jabbs", "lad");
+console.log(user1.debug());
