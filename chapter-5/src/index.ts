6d085034ba656f33c449d61310163d2d1fc4c582
Jamie Atkinson <jabbslad@gmail.com>
2020-02-01 17:50:02 +0000

    add stringdatabase implementation

--- chapter-5/src/index.ts
+++ chapter-5/src/index.ts
@@ -115,3 +115,24 @@ let cat: Cat = new Cat();
 cat.eat("cat food");
 cat.sleep(6);
 cat.meow();
+
+type State = {
+  [key: string]: string;
+};
+
+class StringDatabase {
+  state: State = {};
+  get(key: string): string | null {
+    return key in this.state ? this.state[key] : null;
+  }
+  set(key: string, value: string): void {
+    this.state[key] = value;
+  }
+  static from(state: State): StringDatabase {
+    let db: StringDatabase = new StringDatabase();
+    for (let key in state) {
+      db.set(key, state[key]);
+    }
+    return db;
+  }
+}
