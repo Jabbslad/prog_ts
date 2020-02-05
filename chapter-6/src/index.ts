9a3e13cdea140e2fbcdb7a2f33bd7c5da68a1d06
Jamie Atkinson <jabbslad@gmail.com>
2020-02-05 01:31:19 +0000

    add activitylog implementation

--- chapter-6/src/index.ts
+++ chapter-6/src/index.ts
@@ -150,3 +150,25 @@ function isBig(n: number): boolean {
 function get<O extends object, K extends keyof O>(o: O, k: K): O[K] {
   return o[k];
 }
+
+type ActivityLog = {
+  lastEvent: Date;
+  events: {
+    id: string;
+    timestamp: Date;
+    type: "Read" | "Write";
+  }[];
+};
+
+let activityLog: ActivityLog = {
+  lastEvent: new Date(),
+  events: [
+    {
+      id: "1",
+      timestamp: new Date(),
+      type: "Read"
+    }
+  ]
+};
+
+console.log(get(activityLog, "lastEvent"));
