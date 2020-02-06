6f3888a8a3b75aa1d8e701a4a187d2b941e8bbcb
Jamie Atkinson <jabbslad@gmail.com>
2020-02-06 23:36:14 +0000

    add Get type

--- chapter-6/src/index.ts
+++ chapter-6/src/index.ts
@@ -172,3 +172,13 @@ let activityLog: ActivityLog = {
 };
 
 console.log(get(activityLog, "lastEvent"));
+
+type Get = {
+  <O extends Object, K1 extends keyof O>(o: O, k1: K1): O[K1];
+  <O extends Object, K1 extends keyof O, K2 extends keyof O[K1]>(
+    o: O,
+    k1: K1,
+    k2: K2
+  ): O[K1][K2];
+  <O extends Object, K1 extends keyof O, K2 extends keyof O[K1], K3 extends keyof O[K1][K2](o: O, k1: K1, k2: K2, k3: K3): O[K1][K2][K3]
+};
