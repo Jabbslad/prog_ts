7937303a749463e76353d793a5edccefc6f1bc06
Jamie Atkinson <jabbslad@gmail.com>
2020-02-13 01:01:58 +0000

    add validation of date for error handling

--- chapter-7/src/index.ts
+++ chapter-7/src/index.ts
@@ -8,9 +8,23 @@ function ask() {
   return prompt("when is your birthday? ");
 }
 
-function parse(birthday: string): Date {
-  return new Date(birthday);
+function parse(birthday: string): Date | null {
+    let date = new Date(birthday);
+    if (!isValid(date)) {
+        return null
+    }
+    return date
+}
+
+function isValid(date: Date): boolean {
+    return Object.prototype.toString.call(date) === '[object Date]'
+        && !Number.isNaN(date.getTime())
 }
 
 let date = parse(ask())
-console.info('date is ', date.toISOString())
+
+if(isValid(date)) {
+    console.info('date is ', date.toISOString())
+} else {
+    console.error("error parsing the date for some reason")
+}
