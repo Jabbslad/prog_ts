e88ac64a1f949267ee2817db06259099361949d2
Jamie Atkinson <jabbslad@gmail.com>
2020-02-15 16:18:17 +0000

    rethrowing exceptions

--- chapter-7/src/index.ts
+++ chapter-7/src/index.ts
@@ -47,3 +47,30 @@ try {
         throw e
     }
 }
+
+class InvalidDateFormatError extends RangeError {}
+class DateIsInTheFutureError extends RangeError {}
+
+function parse3(birthday: string): Date {
+    let date = new Date(birthday)
+    if (!isValid(date)) {
+        throw new InvalidDateFormatError("Enter a date in the form YYYY/MM/DD")
+    }
+    if (date.getTime() > Date.now()) {
+        throw new DateIsInTheFutureError("Are you a timelord?")
+    }
+    return date
+}
+
+try {
+    let date = parse3(ask())
+    console.info('date is', date.toISOString())
+} catch(e) {
+    if (e instanceof InvalidDateFormatError) {
+        console.error(e.message)
+    } else if (e instanceof DateIsInTheFutureError) {
+        console.info(e.message)
+    } else {
+        throw e
+    }
+}
