b3dba1995adf589ef0033ee06997e80dbc1614bc
Jamie Atkinson <jabbslad@gmail.com>
2020-02-15 23:20:26 +0000

    implement Option interface

--- chapter-7/src/index.ts
+++ chapter-7/src/index.ts
@@ -96,6 +96,11 @@ if (result instanceof InvalidDateFormatError) {
     console.error(result.message)
 }
 
+interface Option<T> { }
+class Some<T> implements Option<T> {
+    constructor(private value: T)
+}
+class None implements Option<never> { }
 
 
 
