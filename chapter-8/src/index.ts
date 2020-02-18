a793f6cbc414af3511b3092e22a0a663e3bb14b9
Jamie Atkinson <jabbslad@gmail.com>
2020-02-18 01:37:30 +0000

    add call to read apache access log

--- chapter-8/src/index.ts
+++ chapter-8/src/index.ts
@@ -1,3 +1,17 @@
+/*
 setTimeout(() => console.info('A'), 1)
 setTimeout(() => console.info('B'), 2)
 console.info('C')
+*/
+
+import * as fs from 'fs'
+
+fs.readFile('/var/log/apache2/access_log',
+    { encoding: 'utf8' },
+    (error, data) => {
+        if (error) {
+            console.error('error reading!', error)
+            return
+        }
+        console.info('success reading!', data)
+    })
